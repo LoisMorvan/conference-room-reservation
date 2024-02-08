94f518a33567cd6ee01ed8057998f3584a700b64
MORVAN Loïs <lmorvan6@myges.fr>
2024-02-08 16:17:39 +0100

    Login - Création de la route /login - Connexion avec l'email et password de l'utilisateur

--- user/login.mjs
+++ user/login.mjs
@@ -1,6 +1,35 @@
-import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccountKey.json';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { loginSchema } from './validation/loginSchema.mjs';
+import { firebaseApp } from "./config/firebase.mjs";
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+const auth = getAuth(firebaseApp);
+
+export const handler = async (event) => {
+    try {
+        const userData = JSON.parse(event.body);
+        const result = loginSchema.safeParse(userData);
+
+        if (!result.success) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify(result.error),
+            };
+        }
+
+        const { email, password } = result.data;
+
+        // Sign in user with email and password
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Login successful', user: userCredential.user }),
+        };
+    } catch (error) {
+        console.log(error.code);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'The email address or password is incorrect' })
+        };
+    }
+};
